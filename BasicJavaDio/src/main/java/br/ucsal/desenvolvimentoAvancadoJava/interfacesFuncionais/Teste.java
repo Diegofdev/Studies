7828d05c973f9359fa8c976c8af430b0df9ce33c
Diegofdev <diegofdevba@gmail.com>
2021-03-04 21:54:48 -0300

    Updated Studies

--- BasicJavaDio/src/main/java/br/ucsal/desenvolvimentoAvancadoJava/interfacesFuncionais/Teste.java
+++
@@ -1,7 +0,0 @@
-package br.ucsal.desenvolvimentoAvancadoJava.interfacesFuncionais;
-
-public class Teste {
-    public static void main(String[] args) {
-        System.out.println("porra gradle");
-    }
-}
