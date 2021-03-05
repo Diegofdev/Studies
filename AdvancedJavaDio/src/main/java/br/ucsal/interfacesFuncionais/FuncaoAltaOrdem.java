7db8b6d074c829a04a7c99f7adef4c4ff1355b67
Diegofdev <diegofdevba@gmail.com>
2021-03-04 21:58:26 -0300

    updated study

--- AdvancedJavaDio/src/main/java/br/ucsal/interfacesFuncionais/FuncaoAltaOrdem.java
+++ AdvancedJavaDio/src/main/java/br/ucsal/interfacesFuncionais/FuncaoAltaOrdem.java
@@ -1,10 +1,17 @@
-package br.ucsal.desenvolvimentoAvancadoJava.interfacesFuncionais;
+package br.ucsal.interfacesFuncionais;
 
 public class FuncaoAltaOrdem {
     public static void main(String[] args) {
-        Calculo soma = ( a, b) -> a+b;
+        Calculo SOMA = ( a, b) -> a+b;
+        Calculo SUBTRACAO = ( a, b) -> a-b;
+        Calculo DIVISAO = ( a, b) -> a/b;
+        Calculo MULT = ( a, b) -> a*b;
 
-        System.out.println(executarOperacao(soma, 2, 5));
+
+        System.out.println(executarOperacao(SOMA, 2, 5));
+        System.out.println(executarOperacao(SUBTRACAO, 5, 2));
+        System.out.println(executarOperacao(DIVISAO, 5, 2));
+        System.out.println(executarOperacao(MULT, 2, 5));
     }
 
     public static int executarOperacao (Calculo calculo, int a, int b){
