bb5d9ea6941ac44cbf3c22f1cfb688243d8d4e5c
Diegofdev <diegofdevba@gmail.com>
2021-03-04 21:49:55 -0300

    Updated Study

--- BasicJavaDio/src/main/java/br/ucsal/collections_e_Streams/optionals/ExemploOptional.java
+++ BasicJavaDio/src/main/java/br/ucsal/collections_e_Streams/optionals/ExemploOptional.java
@@ -5,23 +5,23 @@ import java.util.Optional;
 public class ExemploOptional {
     public static void main(String[] args) {
 
-        Optional<String> optionalString = Optional.of("Valor opcional");
-
-        System.out.println(optionalString.isPresent());
-        System.out.println(optionalString.isEmpty());
-
-        optionalString.ifPresent(System.out::println);
-
-        optionalString.ifPresentOrElse((System.out::println, () -> System.out.println("Valor não esta presente")));
-
-        if(optionalString.isPresent()){
-            String valor = optionalString.get():
-
-            System.out.println(valor);
-        }
-
-        optionalString.map((valor) -> valor.concat("****")).ifPresentOrElse(System.out::println);
-
-        optionalString.orElseThrow(IllegalStateException::new):
+//        Optional<String> optionalString = Optional.of("Valor opcional");
+//
+//        System.out.println(optionalString.isPresent());
+//        System.out.println(optionalString.isEmpty());
+//
+//        optionalString.ifPresent(System.out::println);
+//
+//        optionalString.ifPresentOrElse((System.out::println, () -> System.out.println("Valor não esta presente")));
+//
+//        if(optionalString.isPresent()){
+//            String valor = optionalString.get():
+//
+//            System.out.println(valor);
+//        }
+//
+//        optionalString.map((valor) -> valor.concat("****")).ifPresentOrElse(System.out::println);
+//
+//        optionalString.orElseThrow(IllegalStateException::new):
     }
 }
---
+++ BasicJavaDio/src/main/java/br/ucsal/desenvolvimentoAvancadoJava/interfacesFuncionais/Teste.java
@@ -0,0 +1,7 @@
+package br.ucsal.desenvolvimentoAvancadoJava.interfacesFuncionais;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("porra gradle");
+    }
+}
